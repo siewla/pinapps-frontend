286452f574143ae521617e994ad5750e5fa4d47d
neocheekiong <neocheekiong@gmail.com>
2020-08-02 13:29:53 +0800

    add apps component

--- src/screens/Apps/AddApp.jsx
+++ src/screens/Apps/AddApp.jsx
@@ -1,24 +1,80 @@
 import React from 'react'
 
-export default function AddApp() {
-    return (
-        <div>
-            <form action="/api/app/new" method="post">
-                <label htmlFor="name">Name of App</label>
-                <input type="text" name="name" id="app-name"/>
-                <label htmlFor="url">URL of App Website</label>
-                <input type="url" name="url" id="app-url"/>
-                <label htmlFor="description">Description of App</label>
-                <textarea name="description" id="app-description" cols="30" rows="10"></textarea>
-                <label htmlFor="category">Category</label>
-                <select name="category" id="app-category">
+class Form extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            products,
+            name:'',
+            price: 0,
+            description: 'Describe this item'
+        }
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit= (event) => {
+        event.preventDefault();
+        console.log('you prevented the default');
+        const newItem = {
+            name: this.state.name,
+            price: this.state.price,
+            description: this.state.description
+        }
+        
+        this.setState({
+            products: [newItem, ...this.state.products]
+        })
+
+        this.setState({
+            products: [ newItem, ...this.state.products],
+            name: '',
+            price: 0,
+            description: 'Describe this item'
+          })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Recommend an App </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">App</label>
+                    <input type="text" value={this.state.name} onChange={this.handleChange} name="name" id='app-name'/>
+                    <label htmlFor="url">Where did you find this App?</label>
+                    <input type="url" name="url" id="app-url" value={this.state.price} onChange={this.handleChange}/>
+                    <label htmlFor="description">How does this app help you? </label>
+                    <input type="text" value={this.state.description} onChange={this.handleChange} id='description'/>
+                    <label htmlFor="category">Category</label>
+                    <select name="category" id="app-category">
                     {/* <option value="productivity">Productivity</option>
                     <option value="timers">Timers</option>
                     <option value="task-management">Task Management</option>
                     dynamically grab from category map.
                      */}
                 </select>
-            </form>
-        </div>
-    )
+                    <input type="submit" value="Enter"/>
+                </form>
+
+                <ul>
+                    {this.state.products.map(product => {
+                        return (
+                            <li>{product.name}  {product.price}</li>)
+                    }
+                    )
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
+
+ReactDOM.render(
+    <Form></Form>, document.querySelector('#app')
+)
+
