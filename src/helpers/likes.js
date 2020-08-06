cd44a7d568e7309b61e12791f9a414353efd9469
neocheekiong <neocheekiong@gmail.com>
2020-08-06 21:47:10 +0800

    updated likes helper

--- src/helpers/likes.js
+++ src/helpers/likes.js
@@ -5,8 +5,21 @@
  * @param {String} userId 
  * @returns {Array} final tally of users who liked the app.
  */
-exports.toggleLikes = (likesArray, userId) => {
+exports.toggleLikesOnApp = (likesArray, userId) => {
     return likesArray.includes(userId) ? likesArray.filter(user => user !== userId) : likesArray.reduce((likes, user) => {
         return likes.push(user);
     }, [userId])
+}
+
+/**
+ * Enter the array of likes as likesArray, and the userId of the user who liked.
+ * Returns an array of the final array of likes.
+ * @param {Array} likesArray 
+ * @param {String} userId 
+ * @returns {Array} final tally of users who liked the app.
+ */
+exports.toggleLikesOnUsers = (likesArray, appId) => {
+    return likesArray.includes(appId) ? likesArray.filter(app => app !== appId) : likesArray.reduce((likes, app) => {
+        return likes.push(app);
+    }, [appId])
 }
