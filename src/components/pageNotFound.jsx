ea03adc5f13c98c47e504a8b3c3250dcb5803cca
siewla <siewlapang@gmail.com>
2020-08-07 16:56:07 +0800

    test new auth

---
+++ src/components/pageNotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class pageNotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Sorry, page not found!</h3>
+            </div>
+        )
+    }
+}
+
+export default pageNotFound
