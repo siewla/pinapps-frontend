414ba6b746d107c062f9f7d2cac01bc89108c1f4
siewla <siewlapang@gmail.com>
2020-08-07 13:27:11 +0800

    max 3 app per category

--- src/components/AppsInHorizontalList.jsx
+++ src/components/AppsInHorizontalList.jsx
@@ -4,7 +4,40 @@ export class AppsInHorizontalList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            apps:null
+            apps:[
+            {
+                name: "AppOne",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                url: "https://www.google.com/",
+                screenshot: "https://images.unsplash.com/photo-1568144628871-ccbb00fc297c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                category: "one",
+                _id: "1234"
+            },
+            {
+                name: "AppTwo",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                url: "https://www.google.com/",
+                screenshot: "https://images.unsplash.com/photo-1568144628871-ccbb00fc297c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                category: "one",
+                _id: "123455"
+            },
+            {
+                name: "AppThree",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                url: "https://www.google.com/",
+                screenshot: "https://images.unsplash.com/photo-1568144628871-ccbb00fc297c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                category: "one",
+                _id: "1234w4"
+            },
+            {
+                name: "AppThree",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                url: "https://www.google.com/",
+                screenshot: "https://images.unsplash.com/photo-1568144628871-ccbb00fc297c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                category: "one",
+                _id: "1234w4"
+            },
+            ]
         }
     }
 
@@ -17,16 +50,17 @@ export class AppsInHorizontalList extends Component {
         this.setState({apps:results})
     }
 
-    componentDidMount (){
-        this.fetchApps()
-    }
+    // componentDidMount (){
+    //     this.fetchApps()
+    // }
 
     render() {
         // console.log(this.state.apps)
         if (this.state.apps!==null){
+            let subsetApps = this.state.apps.slice(0,3)
             return(
                 <div className="horizontal-apps-container">
-                    {this.state.apps.map(app=>
+                    {subsetApps.map((app,index)=>
                     <div key={app._id} className="horizontal-app-card">
                         <img className="horizontal-app-image" src={app.screenshot} key={app._id} alt={app.name}></img> 
                     </div>
