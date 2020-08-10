e6147986f1cbf983311e89c645befd1d9c34dbc1
neocheekiong <neocheekiong@gmail.com>
2020-08-11 00:09:24 +0800

    edit user profile stage 1

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBModal, MDBModalBody, MDBModalHeader } from 'mdbreact';
+import { MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBInput, MDBBtn } from 'mdbreact';
 
 class UserProfile extends Component {
 state = {
-    modal: false
+    modal: false,
+    editName: false,
+    editEmail: false
 }
 
 toggle = () => {
@@ -12,15 +14,27 @@ toggle = () => {
     });
 }
 
+toggleEdit = (key) => () =>{
+    this.setState({
+        [key]: !this.state[key]
+    })
+}
+
 render() {
     return (
         <div>
-            <h4 className="greeting-text">Hi, <span onClick={this.toggle} className="small-name-text">{this.props.user.name}</span></h4>
+            <h4 className="greeting-text">Hi, <span onClick={this.toggle} className="small-name-text" style={{cursor: 'pointer'}}>{this.props.user.name}</span></h4>
             <MDBContainer>
             <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
                 <MDBModalHeader toggle={this.toggle}>Account Details</MDBModalHeader>
                 <MDBModalBody>
-                    <h4><span className="red-text">Name</span> : {this.props.user.name}</h4>  
+                    {this.state.editName ? 
+                        <MDBInput label="name">{this.props.name}</MDBInput> : 
+                        <div>
+                            <h4><span className="red-text">Name</span> : {this.props.user.name}</h4>
+                            <MDBBtn onClick={this.toggleEdit('editName')}>Edit</MDBBtn>
+                        </div> }
+                    
                     <h4><span className="red-text">Email</span> : {this.props.user.email}</h4>   
                 </MDBModalBody>
             </MDBModal>
