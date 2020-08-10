e1d4778632273347d083aced318bd5acb304d055
neocheekiong <neocheekiong@gmail.com>
2020-08-10 23:49:07 +0800

    about page copy written

--- src/screens/notLogin/About.jsx
+++ src/screens/notLogin/About.jsx
@@ -24,7 +24,25 @@ export class About extends Component {
                 "Question": "Question 5",
                 "Answer" : "Answer 5"
             },
-        ]    
+        ],
+        useCase: 
+            <div>
+                <h2>
+                    Have you ever formatted your computer and now you cannot remember which apps you normally use? Especially the apps that you don’t use so often but when you do, you rely 100% on?
+                </h2>
+                <h2>
+                    Have you ever wanted to look for an app for a specific use, but have no idea which app it was that your friend mentioned? Or worse, you have forgotten which was the friend that recommended the app to you in the first place?
+                </h2>
+            </div>,
+        about:  <h3>
+                    <span className="red-text">PinApps</span> is the application that allowing you to save/pin 
+                    your favorite apps easily and it also acts as platform for you 
+                    to find the best apps in each category.
+                </h3>,
+        attribution: <h3>PinsApp is proudly bought to you by <a 
+                        href="https://github.com/neocheekiong/">Neo Chee Kiong</a> and <a 
+                        href="https://github.com/siewla/">Pang Siew La</a>.
+                    </h3>
     }
 
     render() {
@@ -32,15 +50,9 @@ export class About extends Component {
             <div className="main-container">
                 <h1 className="category-heading">About Us</h1>
                 <div className="about-container"> 
-                    <h3>
-                        <span className="red-text">PinApps</span> is the application that allowing you to save/pin 
-                        your favorite apps easily and it also acts as platform for you 
-                        to find the best apps in each category.
-                    </h3>
-                    <h3>PinsApp is proudly bought to you by <a 
-                        href="https://github.com/neocheekiong/">Neo Chee Kiong</a> and <a 
-                        href="https://github.com/siewla/">Pang Siew La</a>.
-                    </h3>
+                    {this.state.useCase}
+                    {this.state.about}
+                    {this.state.attribution}
                 </div>
                 <h1 className="category-heading">Frequently Asked Questions</h1>
                 <div className="about-container">
