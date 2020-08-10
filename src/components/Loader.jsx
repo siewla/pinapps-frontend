37b94591c0e79fd065509769a0de5a10e8a6d0b3
neocheekiong <neocheekiong@gmail.com>
2020-08-10 14:50:22 +0800

    loader added - tweaking needed

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { usePromiseTracker } from 'react-promise-tracker'
+import ReactLoading from 'react-loading'
+
+export default function Loader(props) {
+    const { promiseInProgress } = usePromiseTracker()
+    return (
+        promiseInProgress ? <div>
+            <ReactLoading type={'cylon'} color={'#123456'} width={'100%'}/>
+        </div> :
+        <div>{props.children}</div>
+    )
+}
--- src/screens/Apps/AddApp.jsx
+++ src/screens/Apps/AddApp.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+import {trackPromise} from 'react-promise-tracker'
+import Loader from '../../components/Loader';
 
 const AddApp = (props) => {
     const [formData, setFormData] = useState({
@@ -42,31 +44,34 @@ const AddApp = (props) => {
         event.preventDefault()
         if(name && url && description){
             const userId = JSON.parse(localStorage.getItem('user'));
+            trackPromise(
                 axios.post(`${process.env.REACT_APP_BACKEND_API_URL}/apps/new`,{
                     name, url, description, category, userId
                 })
-                    .then(res =>{
-                        // console.log(res)
-                        setFormData({
-                            ...formData,
-                            name:'',
-                            url:'',
-                            description:'',
-                            category:''
-                        });
-                        window.location.reload()
-                        // this.props.fetchData()
-                        // console.log('added app:',res)
-                    })
-                    .catch(err =>{
-                        setMessage({
-                            message: 'server error, try again'
-                        })
-                    })
-            } else{
-                setMessage({
-                    message:'Please fill in all the fields'
+                .then(res =>{
+                    // console.log(res)
+                    setFormData({
+                        ...formData,
+                        name:'',
+                        url:'',
+                        description:'',
+                        category:''
+                    });
+                    window.location.reload()
+                    // this.props.fetchData()
+                    // console.log('added app:',res)
                 })
+                .catch(err =>{
+                    setMessage({
+                        message: 'server error, try again'
+                    })
+                })
+            )
+                
+        } else {
+            setMessage({
+                message:'Please fill in all the fields'
+            })
         }
     }
 
@@ -76,50 +81,53 @@ const AddApp = (props) => {
                 <MDBRow>
                     <MDBCol>
                         <MDBCard>
-                            <MDBCardBody>
-                            <form>
-                                <p>{message}</p>
-                                <div className="grey-text">
-                                <MDBInput 
-                                    label="Name of App" 
-                                    group 
-                                    type="text" 
-                                    validate 
-                                    error="Please fill in the app's name."
-                                    success="right" 
-                                    onChange={handleChange('name')}
-                                    value={name}
-                                />
-                                <MDBInput 
-                                    label="App Website" 
-                                    group 
-                                    type="url" 
-                                    validate 
-                                    onChange={handleChange('url')}
-                                    value={url}
-                                />
-                                <MDBInput 
-                                    label="App Description" 
-                                    group 
-                                    type="text" 
-                                    validate 
-                                    onChange={handleChange('description')}
-                                    value={description}
-                                />
-                                <label htmlFor="">Category</label>
-                                <select className="browser-default custom-select" value={category} onChange={handleChange('category')}>
-                                    
-                                    <option>Category of App</option>
-                                    {categoryData.allCategories.map(categoryItem => {
-                                        return <option value={categoryItem._id} key={categoryItem._id}>{categoryItem.name}</option>
-                                    })}
-                                </select>
-                                </div>
-                                <div className="text-center">
-                                <MDBBtn color="red" type="submit">Submit</MDBBtn>
-                                </div>
-                            </form>
-                            </MDBCardBody>
+                            <Loader>
+                                <MDBCardBody>
+                                    <form>
+                                        
+                                        <p>{message}</p>
+                                        <div className="grey-text">
+                                        <MDBInput 
+                                            label="Name of App" 
+                                            group 
+                                            type="text" 
+                                            validate 
+                                            error="Please fill in the app's name."
+                                            success="right" 
+                                            onChange={handleChange('name')}
+                                            value={name}
+                                        />
+                                        <MDBInput 
+                                            label="App Website" 
+                                            group 
+                                            type="url" 
+                                            validate 
+                                            onChange={handleChange('url')}
+                                            value={url}
+                                        />
+                                        <MDBInput 
+                                            label="App Description" 
+                                            group 
+                                            type="text" 
+                                            validate 
+                                            onChange={handleChange('description')}
+                                            value={description}
+                                        />
+                                        <label htmlFor="">Category</label>
+                                        <select className="browser-default custom-select" value={category} onChange={handleChange('category')}>
+                                            <option>Category of App</option>
+                                            {categoryData.allCategories.map(categoryItem => {
+                                                return <option value={categoryItem._id} key={categoryItem._id}>{categoryItem.name}</option>
+                                            })}
+                                        </select>
+                                        </div>
+                                        <div className="text-center">
+                                            <MDBBtn color="red" type="submit">Submit</MDBBtn>
+                                        </div>
+                                    </form>
+                                    </MDBCardBody>
+                                </Loader>
+                            
                         </MDBCard>
                     </MDBCol>
                 </MDBRow>
