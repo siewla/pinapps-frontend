10b8d878c01994e776c08475406addeb9ee5195f
siewla <siewlapang@gmail.com>
2020-08-10 15:43:34 +0800

    change loader to red

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,7 +6,7 @@ export default function Loader(props) {
     const { promiseInProgress } = usePromiseTracker()
     return (
         promiseInProgress ? <div>
-            <ReactLoading type={'cylon'} color={'#00ff00'} width={'100%'}/>
+            <ReactLoading type={'cylon'} color={'red'} width={'100%'}/>
         </div> :
         <div>{props.children}</div>
     )
